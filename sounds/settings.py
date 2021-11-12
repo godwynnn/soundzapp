a50549586235dd5597987df6d6f450ec6a3c81a7
godwynnn <78883893+godwynnn@users.noreply.github.com>
2021-11-12 12:23:59 +0100

    Update settings.py

--- sounds/settings.py
+++ sounds/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
