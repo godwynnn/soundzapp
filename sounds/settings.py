5ff50c96dec493e87da52ef899eb450f3dbfb545
godwynnn <78883893+godwynnn@users.noreply.github.com>
2021-11-12 16:03:56 +0100

    Update settings.py

--- sounds/settings.py
+++ sounds/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['soundz-demo.herokuapp.com','127.0.0.1']
 
 
 # Application definition
