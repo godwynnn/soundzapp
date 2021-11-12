e37418138608577943add449b4dd54a6ac188ae4
godwynnn <78883893+godwynnn@users.noreply.github.com>
2021-11-12 12:24:59 +0100

    Update urls.py

--- sounds/urls.py
+++ sounds/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 from sounds.settings import MEDIA_ROOT
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+#     path('admin/', admin.site.urls),
     path('soundz/', include('frontend.urls')),
     path('accounts/', include('allauth.urls')),
 ]
